5fe16c3c17b30afbf14b2b92177fc30b90f08967
username5243 <33399852+username5243@users.noreply.github.com>
2021-03-28 20:10:20 -0400

    Update polygon.js

--- src/extras/polygon.js
+++ src/extras/polygon.js
@@ -1,10 +1,10 @@
 import * as math from "../classes/math.js";
 
-export const polygon = function(n) {
+export const polygon = function(n, m, d, r) {
     const res = [];
 
     for(let i = 0; i < n; i++)
-        res.push([math.cos(i, n), math.sin(i, n)]);
+        res.push([math.cos(i+m, d*n)*r, math.sin(i+m, d*n)*r]);
 
     return res;
 }
